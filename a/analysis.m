c9ca9def0a6b58b6b5cc569acc2bf280ccb78b89
sankalpsharmaa <sharma@devdatalab.org>
2023-10-29 23:41:34 -0700

    adds investment GDP ratio graph

--- a/analysis.m
+++ a/analysis.m
@@ -12,3 +12,25 @@ us_data = readtable(fullfile(mdata, 'us_clean.csv'));
 
 % open UK data
 uk_data = readtable(fullfile(mdata, 'uk_clean.csv'));
+
+% construct investment-GDP ratio
+us_data.IY = us_data.i_t * 100 ./ us_data.y_t;
+uk_data.IY = uk_data.i_t * 100 ./ uk_data.y_t;
+
+savefig = figure
+
+% Plot the series
+plot (us_data.year, us_data.IY)
+hold on 
+
+plot (uk_data.year, uk_data.IY)
+xlabel ( 'Year' ) ;
+ylabel ( '(%)' ) ;
+legend('USA','UK','Interpreter','latex')
+title('$\frac{I_t}{Y_{t}}$','Interpreter','latex');
+
+set(savefig,'Units','Inches');
+pos = get(savefig,'Position');
+
+set(savefig,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(savefig,'Investment','-dpdf','-r0')
